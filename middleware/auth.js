e649777858b36b38ed236c1c88ba57ac7b34c2f2
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-08-10 15:20:12 +0200

    Modified no token provided response

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = function authMiddleware(req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.'); // 401 - no auth credentials
+  if (!token) return res.status(401).send({ code: 401, message: 'Access denied. No token provided.' }); // 401 - no auth credentials
 
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
