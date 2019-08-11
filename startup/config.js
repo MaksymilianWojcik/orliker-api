0a91bded52e4761f5a974b5d92a59e3afcf2c487
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-08-11 17:15:07 +0200

    Moved db to mongo atlas

--- startup/config.js
+++ startup/config.js
@@ -3,6 +3,9 @@ const config = require('config');
 module.exports = function initConfig() {
   if (!config.get('jwtPrivateKey')) {
     // export orliker_privateKey=costamcostamsekretkey
-    throw new Error('FATA ERROR: jwtPrivateKey is not defined');
+    throw new Error('FATA ERROR: orliker_jwtPrivateKey is not defined');
+  }
+  if (!config.get('mongoUser') || !config.get('mongoPassword')) {
+    throw new Error('FATAL ERROR: orliker_mongoPassword or orliker_mongoUser is not defined');
   }
 };
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,15 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function initMongo() {
+  const user = config.get('mongoUser');
+  const password = config.get('mongoPassword');
   mongoose
-    .connect('mongodb://localhost/orliker', { useNewUrlParser: true })
+    .connect(
+      `mongodb+srv://${user}:${password}@orliker-api-iejpl.mongodb.net/test?retryWrites=true&w=majority`,
+      { useNewUrlParser: true }
+    )
     .then(() => winston.info('Connected to MongoDB...'))
     .catch(() => winston.error('Couldnt connect to MongoDB...'));
 };
