3e363928dd22f8052fce2240033125f3b6d87097
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-08-11 17:14:55 +0200

    Commented sockets

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 
+// const { User } = require('./models/user');
+
+// const changeStream = User.watch();
+
+// changeStream.on('change', change => {
+//   console.log('user collection changed');
+// });
+
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-require('./startup/socketio')(server);
+// require('./startup/socketio')(server);
 
 const port = process.env.PORT || 3000; // command: export PORT=5000
-server.listen(port, () => winston.info(`Listening on port ${port} with env: ${app.get('env')}`));
+server.listen(port, () => {
+  const host = server.address().address;
+  winston.info(`Listening on port ${port}, host: ${host} with env: ${app.get('env')}`);
+});
